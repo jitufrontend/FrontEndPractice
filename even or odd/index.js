de9f75ee7bb300b973a901409a7f7c71c42a61a2
jitufrontend <jitendramistry500@gmail.com>
2022-11-23 21:50:58 +0530

    odd even functionality done

--- even or odd/index.js
+++ even or odd/index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+// Write a JavaScript Program whether the given Number is even or odd.
+function evenOdd(){
+    let input = document.getElementById("num").value;
+    let ans = document.getElementById("demo");
+    if(input % 2 == 0){
+        ans.innerHTML = "the number is even";
+    }
+    else{
+        ans.innerHTML = "the number is odd";
+    }
+}

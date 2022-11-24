ed57fca51e9284e826d595e4d251dff5d6831122
jitufrontend <jitendramistry500@gmail.com>
2022-11-24 22:03:32 +0530

    added screen size check

---
+++ screensize/index.js
@@ -0,0 +1,12 @@
+// Write a JavaScript Program whether the given Number is even or odd.
+
+let availheight = screen.availHeight;
+let width = screen.availWidth;
+let height = screen.height;
+let colordepth = screen.colorDepth;
+let pixel = screen.pixelDepth;
+
+let testwidth=document.getElementById("width");
+testwidth.innerHTML=width;
+let testheight=document.getElementById("height");
+testheight.innerHTML=height;
